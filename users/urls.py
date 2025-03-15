5bc9a0932c383bd1174329e673bc0e4f6c2daa6a
mrx <meibrohimov@gmail.com>
2025-03-15 13:27:25 +0500

    add email

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from users.views import RegisterView, LoginView, IndexView
+from users.views import RegisterView, LoginView, IndexView,send_mail_page
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name="register"),
     path('login', LoginView.as_view(), name="login"),
-    path('', IndexView.as_view(), name="index")
+    path('email',send_mail_page,name='email'),
+    path('', IndexView.as_view(), name="index"),
 ]
--- users/views.py
+++ users/views.py
@@ -5,7 +5,9 @@ from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class IndexView(TemplateView):
@@ -38,3 +40,24 @@ class LoginView(FormView):
         else:
             form.add_error(None, 'Invalid username or password')
             return super().form_invalid(form)
+
+
+
+def send_mail_page(request):
+    context = {}
+
+    if request.method == 'POST':
+        address = request.POST.get('address')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        if address and subject and message:
+            try:
+                send_mail(subject, message, settings.EMAIL_HOST_USER, [address])
+                context['result'] = 'Email sent successfully'
+            except Exception as e:
+                context['result'] = f'Error sending email: {e}'
+        else:
+            context['result'] = 'All fields are required'
+    
+    return render(request, "auth/main.html", context)
